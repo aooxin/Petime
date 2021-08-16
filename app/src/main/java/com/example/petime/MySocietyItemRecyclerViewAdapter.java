1a3eead90eb0f78c9fedaa5866289763747733b4
Auswitz_Linux <aosiweixin@outlook.com>
2021-08-16 10:53:20 +0800

    empty commit

--- app/src/main/java/com/example/petime/MySocietyItemRecyclerViewAdapter.java
+++ app/src/main/java/com/example/petime/MySocietyItemRecyclerViewAdapter.java
@@ -1,49 +1,54 @@
 package com.example.petime;
 
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.example.petime.placeholder.PlaceholderContent.PlaceholderItem;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.petime.databinding.FragmentItemBinding;
+import com.example.petime.placeholder.PlaceholderContent.PlaceholderItem;
 
 import java.util.List;
 
 /**
  * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
  * TODO: Replace the implementation with code for your data type.
+ * 社交页面的adapter
  */
 public class MySocietyItemRecyclerViewAdapter extends RecyclerView.Adapter<MySocietyItemRecyclerViewAdapter.ViewHolder> {
 
-    private final List<PlaceholderItem> mValues;
+    private final List<SocietyData> mValues;
 
-    public MySocietyItemRecyclerViewAdapter(List<PlaceholderItem> items) {
+    public MySocietyItemRecyclerViewAdapter(List<SocietyData> items) {
+        //把数据源传进来，并赋给变量mValues
         mValues = items;
     }
-
+//由于Adapter是继承的，所以要重写以下三个方法
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
+//创建view—holder实例
         return new ViewHolder(FragmentItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
 
     }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
-        holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).id);
-        holder.mContentView.setText(mValues.get(position).content);
+        //对子项赋值😘
+        SocietyData mItem=mValues.get(position);
+        //holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mItem.getId());
+        holder.mContentView.setText(mItem.getName());
     }
 
     @Override
     public int getItemCount() {
+        //返回实例长度
         return mValues.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        // 获取我们的实例
         public final TextView mIdView;
         public final TextView mContentView;
         public PlaceholderItem mItem;
