63e307d69b5ce6a21de2f17ddbc3c54f805ac4fa
fang <875470584@qq.com>
2021-08-17 20:06:25 +0800

    无数据库、经验值的小动物wffff

--- app/src/main/java/com/example/petime/pethouse_activity.java
+++ app/src/main/java/com/example/petime/pethouse_activity.java
@@ -1,91 +1,117 @@
 package com.example.petime;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SlidingDrawer;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.List;
+import com.airbnb.lottie.LottieAnimationView;
 
 public class pethouse_activity extends Activity {
-    private GridView gv;
-    private SlidingDrawer sd;
-    private ImageView iv;
-    private List<ResolveInfo> apps;
+    private SlidingDrawer mDrawer;
+    private ImageButton imbg;
+    private Boolean flag=false;
+    private LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pet_house);
-        loadApps();
-        gv = (GridView) findViewById(R.id.allApps);
-        sd = (SlidingDrawer) findViewById(R.id.sliding);
-        iv = (ImageView) findViewById(R.id.imageViewIcon);
-        gv.setAdapter(new GridAdapter());
-        sd.setOnDrawerOpenListener(new SlidingDrawer.OnDrawerOpenListener()// 开抽屉
+
+        imbg=(ImageButton)findViewById(R.id.handle);//button
+        mDrawer=(SlidingDrawer)findViewById(R.id.slidingdrawer);//drawer
+
+        mDrawer.setOnDrawerOpenListener(new SlidingDrawer.OnDrawerOpenListener()
         {
             @Override
             public void onDrawerOpened() {
-                iv.setImageResource(R.drawable.pet1);// 响应开抽屉事件
-                // ，把图片设为向下的
+                flag=true;
+                imbg.setImageResource(R.drawable.ic_pet);
             }
+
         });
-        sd.setOnDrawerCloseListener(new SlidingDrawer.OnDrawerCloseListener() {
+
+        mDrawer.setOnDrawerCloseListener(new SlidingDrawer.OnDrawerCloseListener(){
             @Override
             public void onDrawerClosed() {
-                iv.setImageResource(R.drawable.pet1);// 响应关抽屉事件
+                flag=false;
+                imbg.setImageResource(R.drawable.ic_pet);
             }
+
         });
 
-    }
-
-    private void loadApps() {
-        Intent intent = new Intent(Intent.ACTION_MAIN, null);
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-        apps = getPackageManager().queryIntentActivities(intent, 0);
-    }
-
-    public class GridAdapter extends BaseAdapter {
-        public GridAdapter() {
-
-        }
-
-        public int getCount() {
-            // TODO Auto-generated method stub
-            return apps.size();
-        }
-
-        public Object getItem(int position) {
-            // TODO Auto-generated method stub
-            return apps.get(position);
-        }
-
-        public long getItemId(int position) {
-            // TODO Auto-generated method stub
-            return position;
-        }
-
-        public View getView(int position, View convertView, ViewGroup parent) {
-            // TODO Auto-generated method stub
-            ImageView imageView = null;
-            if (convertView == null) {
-                imageView = new ImageView(pethouse_activity.this);
-                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-                imageView.setLayoutParams(new GridView.LayoutParams(50, 50));
-            } else {
-                imageView = (ImageView) convertView;
+        linearLayout=(LinearLayout)findViewById(R.id.content_pet);
+//        LottieAnimationView lottie_cat=(LottieAnimationView )linearLayout.findViewById(R.id.lottie_cat);
+        LottieAnimationView lottie_cat=(LottieAnimationView )findViewById(R.id.lottie_cat);
+        lottie_cat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(pethouse_activity.this, "You choose cat", Toast.LENGTH_SHORT).show();
+                LottieAnimationView lottie=(LottieAnimationView )findViewById(R.id.lottie);
+//                lottie.setImageAssetsFolder("/home/wufang/androidpetime/Petime/app/src/main/res/raw");
+                lottie.setAnimation("cat.json");
+                lottie.playAnimation();
             }
-            ResolveInfo ri = apps.get(position);
-            imageView.setImageDrawable(ri.activityInfo
-                    .loadIcon(getPackageManager()));
-            return imageView;
-        }
-
+        });
+        LottieAnimationView lottie_rabbit=(LottieAnimationView )findViewById(R.id.lottie_rabbit);
+        lottie_rabbit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(pethouse_activity.this, "You choose rabbit", Toast.LENGTH_SHORT).show();
+                LottieAnimationView lottie=(LottieAnimationView )findViewById(R.id.lottie);
+//                lottie.setImageAssetsFolder("/home/wufang/androidpetime/Petime/app/src/main/res/raw");
+                lottie.setAnimation("rabbit.json");
+                lottie.playAnimation();
+            }
+        });
+        LottieAnimationView lottie_fox=(LottieAnimationView )findViewById(R.id.lottie_fox);
+        lottie_fox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(pethouse_activity.this, "You choose fox", Toast.LENGTH_SHORT).show();
+                LottieAnimationView lottie=(LottieAnimationView )findViewById(R.id.lottie);
+//                lottie.setImageAssetsFolder("/home/wufang/androidpetime/Petime/app/src/main/res/raw");
+                lottie.setAnimation("fox.json");
+                lottie.playAnimation();
+            }
+        });
+        LottieAnimationView lottie_dog=(LottieAnimationView )findViewById(R.id.lottie_dog);
+        lottie_dog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(pethouse_activity.this, "You choose dog", Toast.LENGTH_SHORT).show();
+                LottieAnimationView lottie=(LottieAnimationView )findViewById(R.id.lottie);
+//                lottie.setImageAssetsFolder("/home/wufang/androidpetime/Petime/app/src/main/res/raw");
+                lottie.setAnimation("dog.json");
+                lottie.playAnimation();
+            }
+        });
+        LottieAnimationView lottie_unicorn=(LottieAnimationView )findViewById(R.id.lottie_unicorn);
+        lottie_unicorn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(pethouse_activity.this, "You choose unicorn", Toast.LENGTH_SHORT).show();
+                LottieAnimationView lottie=(LottieAnimationView )findViewById(R.id.lottie);
+//                lottie.setImageAssetsFolder("/home/wufang/androidpetime/Petime/app/src/main/res/raw");
+                lottie.setAnimation("unicorn.json");
+                lottie.playAnimation();
+            }
+        });
+        LottieAnimationView lottie_squirrel=(LottieAnimationView )findViewById(R.id.lottie_squirrel);
+        lottie_squirrel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(pethouse_activity.this, "You choose squirrel", Toast.LENGTH_SHORT).show();
+                LottieAnimationView lottie=(LottieAnimationView )findViewById(R.id.lottie);
+//                lottie.setImageAssetsFolder("/home/wufang/androidpetime/Petime/app/src/main/res/raw");
+                lottie.setAnimation("squirrel.json");
+                lottie.playAnimation();
+            }
+        });
     }
-
 }
